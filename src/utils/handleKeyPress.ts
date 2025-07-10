4dbe68deceae38dcab37ae3e3cbc061dda762d4a
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-07-10 11:56:54 +0300

    feat: Optimized the utils

---
+++ src/utils/handleKeyPress.ts
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const handleNumberKeyPress = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const charCode = e.which || e.keyCode;
+
+    if (charCode < 48 || charCode > 57) {
+      e.preventDefault();
+    }
+}
+
+export const handleFloatKeyPress = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+  const charCode = e.which || e.keyCode;
+  
+  if ((charCode < 48 || charCode > 57) && charCode != 46 && charCode > 31) {
+    e.preventDefault();
+  }
+}
